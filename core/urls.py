483510d59987d7e8f647d4a26555e7fba266a83b
Richelly Italo <richellyitalo@gmail.com>
2019-10-22 09:48:41 -0300

    organizado layout

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import item_list
+from .views import home, products, checkout
 
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', home, name='home'),
+    path('products', products, name='products'),
+    path('checkout', checkout, name='checkout')
 ]
