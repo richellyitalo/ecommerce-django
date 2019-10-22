87ddb14c30d092a5dd0d1c67d1583568ea167ed9
Richelly Italo <richellyitalo@gmail.com>
2019-10-22 19:02:53 -0300

    preco com desconto; slug do item

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Item, Order, OrderItem
 
-admin.site.register(Item)
+
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
--- core/models.py
+++ core/models.py
@@ -19,9 +19,11 @@ LABEL_CHOICES = (
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
