ca49274e3ea64be8a0488ec6671a4a003dd9236d
Richelly Italo <richellyitalo@gmail.com>
2019-10-27 01:04:51 -0300

    pagina resumo de pedidos

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import HomeView, ProductDetail, ProductView, checkout,\
-    add_to_cart, remove_from_cart
+    add_to_cart, remove_from_cart, OrderSummaryView
 
 
 app_name = 'core'
@@ -12,4 +12,5 @@ urlpatterns = [
     path('checkout', checkout, name='checkout'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('order-summary', OrderSummaryView.as_view(), name='order-summary'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from .models import Item, OrderItem, Order
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomeView(ListView):
@@ -22,10 +25,24 @@ class ProductDetail(DetailView):
     template_name = 'product.html'
 
 
+class OrderSummaryView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        try:
+            context = {
+                'order': Order.objects.get(user=request.user, ordered=False)
+            }
+            return render(request, 'order_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "Você não possui nenhum pedido ativo")
+            return redirect('/') 
+
+
 def checkout(request):
     return render(request, 'checkout-page.html')
 
 
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(
@@ -52,6 +69,7 @@ def add_to_cart(request, slug):
     return redirect('core:product', slug=item.slug)
 
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
